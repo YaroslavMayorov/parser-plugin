1805bf97266cd8da1ab860c371f8d52e70c16073
GitHub Action <action@github.com>
2025-10-07 15:08:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/yaroslavmayorov/parserplugin/MyBundle.kt
+++ src/main/kotlin/com/github/yaroslavmayorov/parserplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yaroslavmayorov.parserplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/yaroslavmayorov/parserplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yaroslavmayorov/parserplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yaroslavmayorov.parserplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yaroslavmayorov.parserplugin.MyBundle
+import com.github.yaroslavmayorov.parserplugin.services.MyProjectService
 import javax.swing.JButton
 
 
